84d5f71a97ac3c68d2b7920ffefe57673eeb976e
timrobuck <trobuck@cafepress.com>
2016-09-06 10:33:53 -0400

    add page object

---
+++ Autodan/src/pages/MerchTool/MtMerchandiseColorsPageObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodan.core;
+using OpenQA;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace Autodan.pages.MerchTool
+{
+    internal class MtMerchandiseColorsPageObject:BaseTest
+    {
+
+        public MtMerchandiseColorsPageObject()
+        {
+            PageFactory.InitElements(BaseTest.Driver, this);
+        }
+
+
+       
+    }
+}
