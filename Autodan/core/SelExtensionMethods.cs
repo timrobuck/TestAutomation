6b2eb35286eae50b02e39a78437ad4e2043e7d25
timrobuck <trobuck@cafepress.com>
2016-10-19 08:33:11 -0400

    post script

--- Autodan/core/SelExtensionMethods.cs
+++ Autodan/core/SelExtensionMethods.cs
@@ -1,12 +1,12 @@
 ﻿using OpenQA.Selenium;
-﻿using System;
-using System.Linq;
-using System.Threading;
- using OpenQA.Selenium.Support.UI;
- using System.Collections.Generic;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq.Expressions;
+using System.Linq;
 using System.Text;
+using System.Threading;
+using static System.Console;
 
 namespace Autodan.core
 {
@@ -14,12 +14,12 @@ namespace Autodan.core
     {
         // ReSharper disable once NotAccessedField.Local //need this to hide noise from resharper
         private static Type _type;
-        
+
         public static void EnterText(this IWebElement element, string value)
         {
             element.SendKeys(value);
         }
-       
+
         // click, clear and enter text on an input
         public static void ClearAndEnterText(this IWebElement element, string value)
         {
@@ -27,7 +27,7 @@ namespace Autodan.core
             element.Clear();
             element.SendKeys(value);
         }
-        
+
         // Selecting a dropdown control
         public static void SelectDropdown(this IWebElement element, string value)
         {
@@ -43,7 +43,7 @@ namespace Autodan.core
         // verify presence of an element in dom; smoke/sanity tests
         public static void Verify(this IWebElement element)
         {
-           _type = element.GetType();
+            _type = element.GetType();
         }
         // verify presence of element: if used with 'debugger.isattched' condition and run debug from test runner, this can give a list of all failures in a test suite on single run
         public static bool VerifyOrSendMessage(this IWebElement element)
@@ -55,10 +55,9 @@ namespace Autodan.core
             catch (Exception ex)
             {
                 var sb = new StringBuilder();
-                sb.Append(" ERROR!   ");
-                sb.Append(ex.Message);
+                sb.Append(" ERROR!   ").Append(ex.Message);
                 if (!Debugger.IsAttached) throw new Exception(sb.ToString());
-                Console.WriteLine(sb.ToString());
+                WriteLine(sb.ToString());
                 return false;
             }
             return true;
@@ -70,29 +69,22 @@ namespace Autodan.core
         //// </summary>
         public static bool VerifyElementDependentOnOptionalElement(this IWebElement requiredDependent, IWebElement optional)
         {
-            try {_type = optional.GetType();}
-            catch{return false;}
+            try { _type = optional.GetType(); }
+            catch { return false; }
 
             try { requiredDependent.Verify(); }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 var sb = new StringBuilder();
-                sb.Append(" ERROR!   ");
-                sb.Append(ex.Message);
-                sb.Append(" ");
-                sb.Append(nameof(requiredDependent));
-                sb.Append(" is dependent on ");
-                sb.Append(nameof(optional));
-                sb.Append(" and is required! ");
-
+                sb.Append(" ERROR!   ").Append(ex.Message).Append("Element is dependent and required on if optional element is present");
                 if (!Debugger.IsAttached) throw new Exception(sb.ToString());
-                Console.WriteLine();
+                WriteLine();
                 return false;
             }
             return true;
         }
 
-        public static void VerifyTextIsInThisElement(this List<IWebElement> elements, int orderOfOccuranceOnPage,string targetContent)
+        public static void VerifyTextIsInThisElement(this List<IWebElement> elements, int orderOfOccuranceOnPage, string targetContent)
         {
             if (!elements[orderOfOccuranceOnPage - 1].Text.Contains(targetContent))
                 throw new Exception("targetText=" + targetContent + " was not found for this element.");
@@ -101,7 +93,7 @@ namespace Autodan.core
         public static void VerifyAndVerifyExpectedContent(this IWebElement element, string targetText)
         {
             _type = element.GetType();
-            VerifyTextIsInThisElement(element,targetText);
+            VerifyTextIsInThisElement(element, targetText);
         }
 
         public static int VerifyContentOfThisElementIsIntegerType(this IWebElement element)
@@ -112,12 +104,12 @@ namespace Autodan.core
         public static void VerifyTextIsInThisElement(this IWebElement element, string targetContent)
         {
             if (!element.Text.Contains(targetContent))
-                throw new Exception("element.text=" +element.Text + " but targetText=" + targetContent + " does not match.");
+                throw new Exception("element.text=" + element.Text + " but targetText=" + targetContent + " does not match.");
         }
 
         public static void VerifyContentNotEmpty(this IWebElement element)
         {
-            if(string.IsNullOrEmpty(element.Text))
+            if (string.IsNullOrEmpty(element.Text))
                 throw new Exception(" Empty String : Content Value is Missing ");
         }
 
@@ -132,7 +124,7 @@ namespace Autodan.core
                 return false;
             }
         }
-        
+
         public static void ClickAndWait(this IWebElement element, IWebDriver driver, int seconds)
         {
             element.Click();
